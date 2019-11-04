e14ca2ebfc803395ca31eeb229392de7fe563b84
Dimitris Skoutas <dskoutas@imis.athena-innovation.gr>
2019-11-04 18:54:55 +0200

    Cleaned up old files.

--- deprecated/loci-java/src/main/java/gr/athenarc/imsi/slipo/analytics/loci/score/ScoreFunction.java
+++
@@ -1,9 +0,0 @@
-package gr.athenarc.imsi.slipo.analytics.loci.score;
-
-import java.util.List;
-
-import gr.athenarc.imsi.slipo.analytics.loci.SpatialObject;
-
-public abstract class ScoreFunction<T extends SpatialObject> {
-	public abstract double computeScore(List<T> objects);
-}
