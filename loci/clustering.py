b90530ed92b9fdd7eed7ebce6abff7cfb0e5d4ab
Dimitris Skoutas <dskoutas@imis.athena-innovation.gr>
2019-11-26 19:19:36 +0200

    Updated functionalities for cluster shapes and comparison. Updated examples.

--- loci/clustering.py
+++ loci/clustering.py
@@ -1,25 +1,29 @@
-from pandas import DataFrame, merge
+import pandas as pd
+import geopandas
+
+from pandas import merge
 from time import time
 from sklearn.cluster import DBSCAN
 import numpy as np
 from shapely.ops import cascaded_union
 from geopandas import GeoDataFrame
 from hdbscan import HDBSCAN
+from shapely.geometry import MultiPoint
 
 
-def compute_clusters(pois, alg='hdbscan', min_pts=None, eps=None, cluster_shapes=True):
+def compute_clusters(pois, alg='dbscan', min_pts=None, eps=None, n_jobs=1):
     """Computes clusters using the DBSCAN or the HDBSCAN algorithm.
 
     Args:
          pois (GeoDataFrame): A POI GeoDataFrame.
-         alg (string): The clustering algorithm to use (dbscan or hdbscan; default: hdbscan).
+         alg (string): The clustering algorithm to use (dbscan or hdbscan; default: dbscan).
          min_pts (integer): The minimum number of neighbors for a dense point.
          eps (float): The neighborhood radius.
-         cluster_shapes (Boolean): Whether to return cluster shapes (default: True).
+         n_jobs (integer): Number of parallel jobs to run in the algorithm (default: 1)
 
     Returns:
-          A GeoDataFrame containing the clustered POIs and their labels, a GeoDataFrame containing the POIs that are
-          not part of any cluster, and (optionally) a GeoDataFrame containing the cluster shapes.
+          A GeoDataFrame containing the clustered POIs and their labels. The value of parameter `eps` for each cluster
+          is also returned (which varies in the case of HDBSCAN).
     """
 
     # Prepare list of coordinates
@@ -30,7 +34,7 @@ def compute_clusters(pois, alg='hdbscan', min_pts=None, eps=None, cluster_shapes
     # Compute the clusters
     t0 = time()
     if alg == 'hdbscan':
-        clusterer = HDBSCAN(min_cluster_size=min_pts, min_samples=min_pts)
+        clusterer = HDBSCAN(min_cluster_size=min_pts, min_samples=min_pts, core_dist_n_jobs=n_jobs)
         labels = clusterer.fit_predict(data_arr)
         num_of_clusters = len(set(labels))
 
@@ -38,12 +42,13 @@ def compute_clusters(pois, alg='hdbscan', min_pts=None, eps=None, cluster_shapes
         cluster_tree = tree[tree.child_size > 1]
         chosen_clusters = clusterer.condensed_tree_._select_clusters()
 
-        cluster_tree_chosen = cluster_tree[cluster_tree.child.isin(chosen_clusters)].drop("parent", axis=1).drop("child", axis=1).reset_index().drop("index", axis=1)
-        cluster_tree_chosen['lambda_val'] = cluster_tree_chosen['lambda_val'].apply(lambda x: 1 / x)
-        cluster_tree_chosen.rename(columns={'lambda_val': 'eps', 'child_size': 'cluster_size'}, inplace=True)
+        eps_per_cluster = cluster_tree[cluster_tree.child.isin(chosen_clusters)].\
+            drop("parent", axis=1).drop("child", axis=1).reset_index().drop("index", axis=1)
+        eps_per_cluster['lambda_val'] = eps_per_cluster['lambda_val'].apply(lambda x: 1 / x)
+        eps_per_cluster.rename(columns={'lambda_val': 'eps', 'child_size': 'cluster_size'}, inplace=True)
 
     else:
-        clusterer = DBSCAN(eps=eps, min_samples=min_pts).fit(data_arr)
+        clusterer = DBSCAN(eps=eps, min_samples=min_pts, n_jobs=n_jobs).fit(data_arr)
         labels = clusterer.labels_
 
         num_of_clusters = len(set(labels))
@@ -51,8 +56,8 @@ def compute_clusters(pois, alg='hdbscan', min_pts=None, eps=None, cluster_shapes
         num_of_clusters_no_noise.discard(-1)
         num_of_clusters_no_noise = len(num_of_clusters_no_noise)
 
-        cluster_tree_chosen = DataFrame({'eps': [eps] * num_of_clusters_no_noise})
-        cluster_tree_chosen['cluster_size'] = 0
+        eps_per_cluster = pd.DataFrame({'eps': [eps] * num_of_clusters_no_noise})
+        eps_per_cluster['cluster_size'] = 0
 
     print("Done in %0.3fs." % (time() - t0))
 
@@ -67,11 +72,26 @@ def compute_clusters(pois, alg='hdbscan', min_pts=None, eps=None, cluster_shapes
     print('Number of clustered POIs: %d' % (len(pois_in_clusters)))
     print('Number of outlier POIs: %d' % (len(pois_noise)))
 
-    cluster_borders = None
-    if cluster_shapes:
-        # Compute cluster shapes
-        cluster_borders = pois_in_clusters.groupby(['cluster_id'], sort=False)['geometry'].agg([list, np.size])
-        join_df = merge(cluster_borders, cluster_tree_chosen, left_index=True, right_index=True, how='inner')
+    return pois_in_clusters, eps_per_cluster
+
+
+def cluster_shapes(pois, shape_type=1, eps_per_cluster=None):
+    """Computes cluster shapes.
+
+    Args:
+         pois (GeoDataFrame): The clustered POIs.
+         shape_type (integer): The methods to use for computing cluster shapes (allowed values: 1-3).
+         eps_per_cluster (DataFrame): The value of parameter eps used for each cluster (required by methods 2 and 3).
+
+    Returns:
+          A GeoDataFrame containing the cluster shapes.
+    """
+
+    t0 = time()
+
+    if shape_type == 2:
+        cluster_borders = pois.groupby(['cluster_id'], sort=False)['geometry'].agg([list, np.size])
+        join_df = merge(cluster_borders, eps_per_cluster, left_index=True, right_index=True, how='inner')
         cluster_list = []
         for index, row in join_df.iterrows():
             eps = row['eps']
@@ -82,10 +102,73 @@ def compute_clusters(pois, alg='hdbscan', min_pts=None, eps=None, cluster_shapes
             cluster_list.append(cascaded_union(cluster_i))
 
         join_df['geometry'] = cluster_list
+        join_df['cluster_id'] = join_df.index
         join_df.reset_index(drop=True, inplace=True)
         join_df.drop(['list', 'cluster_size'], axis=1, inplace=True)
 
         cluster_borders = GeoDataFrame(join_df, crs=pois.crs, geometry='geometry')
-        cluster_borders['cluster_id'] = cluster_borders.index
+        cluster_borders = cluster_borders[['cluster_id', 'size', 'geometry']]
 
-    return pois_in_clusters, pois_noise, cluster_borders
+    elif shape_type == 3:
+        eps_dict = dict()
+        for index, row in eps_per_cluster.iterrows():
+            eps_dict[index] = row['eps']
+
+        circles_from_pois = pois.copy()
+        cid_size_dict = dict()
+        circles = []
+        for index, row in circles_from_pois.iterrows():
+            cid = row['cluster_id']
+            circles.append(row['geometry'].buffer(eps_dict[cid]))
+            cid_size_dict[cid] = cid_size_dict.get(cid, 0) + 1
+
+        circles_from_pois['geometry'] = circles
+
+        s_index = pois.sindex
+
+        pois_in_circles = geopandas.sjoin(pois, circles_from_pois, how="inner", op='intersects')
+        agged_pois_per_circle = pois_in_circles.groupby(['cluster_id_left', 'index_right'],
+                                                        sort=False)['geometry'].agg([list])
+
+        poly_list = []
+        cluster_id_list = []
+        for index, row in agged_pois_per_circle.iterrows():
+            pois_in_circle = row['list']
+            lsize = len(pois_in_circle)
+            if lsize >= 3:
+                poly = MultiPoint(pois_in_circle).convex_hull
+                poly_list.append(poly)
+                cluster_id_list.append(index[0])
+
+        temp_df = pd.DataFrame({
+            'cluster_id': cluster_id_list,
+            'geometry': poly_list
+        })
+
+        grouped_poly_per_cluster = temp_df.groupby(['cluster_id'], sort=False)['geometry'].agg([list])
+
+        cluster_size_list = []
+        poly_list = []
+        for index, row in grouped_poly_per_cluster.iterrows():
+            poly_list.append(cascaded_union(row['list']))
+            cluster_size_list.append(cid_size_dict[index])
+
+        grouped_poly_per_cluster['geometry'] = poly_list
+        grouped_poly_per_cluster.drop(['list'], axis=1, inplace=True)
+
+        cluster_borders = GeoDataFrame(grouped_poly_per_cluster, crs=pois.crs, geometry='geometry')
+        cluster_borders['cluster_id'] = cluster_borders.index
+        cluster_borders['size'] = cluster_size_list
+
+    # type == 1 (default)
+    else:
+        cluster_borders = pois.groupby(['cluster_id'], sort=False)['geometry'].agg([list, np.size])
+        cluster_borders['list'] = [MultiPoint(l).convex_hull for l in cluster_borders['list']]
+        cluster_borders.rename(columns={"list": "geometry"}, inplace=True)
+        cluster_borders.sort_index(inplace=True)
+        cluster_borders = GeoDataFrame(cluster_borders, crs=pois.crs, geometry='geometry')
+        cluster_borders.reset_index(inplace=True)
+
+    print("Done in %0.3fs." % (time() - t0))
+
+    return cluster_borders
--- loci/plots.py
+++ loci/plots.py
@@ -9,6 +9,9 @@ from folium.plugins import MarkerCluster
 from loci.analytics import bbox, kwds_freq
 from wordcloud import WordCloud
 from pysal.viz.mapclassify import Natural_Breaks
+from pandas import DataFrame, concat
+from geopandas import GeoDataFrame
+import osmnx
 
 
 def map_points(pois, tiles='OpenStreetMap', width='100%', height='100%', show_bbox=False):
@@ -327,5 +330,295 @@ def map_clusters_with_topics(clusters_topics, viz_type='dominant', col_id='clust
     else:
         clusters_topics['style'] = clusters_topics.apply(lambda row: style_gen_dominant(row), axis=1)
         m = map_geometries(clusters_topics, tiles=tiles, width=width, height=height)
+
+    return m
+
+
+def map_cluster_diff(clusters_a, clusters_b, intersection_color='#00ff00', diff_ab_color='#0000ff',
+                     diff_ba_color='#ff0000', tiles='OpenStreetMap', width='100%', height='100%'):
+    """Returns a Folium Map displaying the differences between two sets of clusters. Map center and zoom level
+    are set automatically.
+
+    Args:
+         clusters_a (GeoDataFrame): The first set of clusters.
+         clusters_b (GeoDataFrame): The second set of clusters.
+         intersection_color (color code): The color to use for A & B.
+         diff_ab_color (color code): The color to use for A - B.
+         diff_ba_color (color code): The color to use for B - A.
+         tiles (string): The tiles to use for the map (default: `OpenStreetMap`).
+         width (integer or percentage): Width of the map in pixels or percentage (default: 100%).
+         height (integer or percentage): Height of the map in pixels or percentage (default: 100%).
+
+    Returns:
+        A Folium Map object displaying cluster intersections and differences.
+    """
+
+    if clusters_a.crs['init'] != '4326':
+        clusters_a = clusters_a.to_crs({'init': 'epsg:4326'})
+
+    if clusters_b.crs['init'] != '4326':
+        clusters_b = clusters_b.to_crs({'init': 'epsg:4326'})
+
+    spatial_index_b = clusters_b.sindex
+    prev_size_list = []
+    prev_cid_list = []
+
+    after_cid_list = []
+    after_size_list = []
+    intersect_area_percentage_list = []
+
+    intersection_polygons = []
+    diff_ab_polygs = []
+    diff_ba_polygs = []
+    new_polygs = []
+
+    intersection_polygons_attr = []
+    diff_ab_polygs_attr = []
+    diff_ba_polygs_attr = []
+    new_polygs_attr = []
+
+    for index_1, row_1 in clusters_a.iterrows():
+        size = row_1['size']
+        poly = row_1['geometry']
+        cid = row_1['cluster_id']
+
+        prev_area = poly.area
+        prev_cid_list.append(cid)
+        prev_size_list.append(size)
+
+        possible_matches_index = list(spatial_index_b.intersection(poly.bounds))
+        possible_matches = clusters_b.iloc[possible_matches_index]
+
+        max_area = 0.0
+        max_cid_intersect = -1
+        max_size = 0
+
+        max_polyg = None
+        max_intersect_polyg = None
+
+        for index_2, row_2 in possible_matches.iterrows():
+            size_2 = row_2['size']
+            poly_2 = row_2['geometry']
+            cid_2 = row_2['cluster_id']
+
+            intersect_polyg = poly.intersection(poly_2)
+            area = intersect_polyg.area
+
+            if area > max_area:
+                max_area = area
+                max_cid_intersect = cid_2
+                max_size = size_2
+                max_polyg = poly_2
+                max_intersect_polyg = intersect_polyg
+
+        if max_cid_intersect == -1:
+            after_cid_list.append(np.nan)
+            after_size_list.append(np.nan)
+            intersect_area_percentage_list.append(0.0)
+            new_polygs.append(poly)
+            new_polygs_attr.append('A (' + str(cid) + ')')
+        else:
+            after_cid_list.append(max_cid_intersect)
+            after_size_list.append(max_size)
+            intersect_area_percentage_list.append(max_area / prev_area)
+
+            ab_diff_poly = poly.difference(max_polyg)
+            ba_diff_poly = max_polyg.difference(poly)
+
+            intersection_polygons.append(max_intersect_polyg)
+            diff_ab_polygs.append(ab_diff_poly)
+            diff_ba_polygs.append(ba_diff_poly)
+
+            intersection_polygons_attr.append('A(' + str(cid) + ') & B(' + str(max_cid_intersect) + ')')
+            diff_ab_polygs_attr.append('A(' + str(cid) + ') - B(' + str(max_cid_intersect) + ')')
+            diff_ba_polygs_attr.append('B(' + str(max_cid_intersect) + ') - A(' + str(cid) + ')')
+
+    spatial_index_a = clusters_a.sindex
+    old_polys = []
+    old_poly_attr = []
+
+    for index, row in clusters_b.iterrows():
+        poly = row['geometry']
+        cid = row['cluster_id']
+
+        possible_matches_index = list(spatial_index_a.intersection(poly.bounds))
+        possible_matches = clusters_a.iloc[possible_matches_index]
+
+        max_area = 0.0
+
+        for index_2, row_2 in possible_matches.iterrows():
+            poly_2 = row_2['geometry']
+
+            intersect_polyg = poly.intersection(poly_2)
+            area = intersect_polyg.area
+
+            if area > max_area:
+                max_area = area
+
+        if max_area == 0.0:
+            old_polys.append(poly)
+            old_poly_attr.append('B (' + str(cid) + ')')
+
+    intersection_gdf = GeoDataFrame(list(zip(intersection_polygons, intersection_polygons_attr)),
+                                    columns=['geometry', 'diff'], crs=clusters_a.crs)
+
+    diff_ab_gdf = GeoDataFrame(list(zip(diff_ab_polygs + new_polygs, diff_ab_polygs_attr + new_polygs_attr)),
+                               columns=['geometry', 'diff'], crs=clusters_a.crs)
+
+    diff_ba_gdf = GeoDataFrame(list(zip(diff_ba_polygs + old_polys, diff_ba_polygs_attr + old_poly_attr)),
+                               columns=['geometry', 'diff'], crs=clusters_a.crs)
+
+    # Filter out erroneous rows
+    intersection_gdf = intersection_gdf[(intersection_gdf.geometry.area > 0.0)]
+    diff_ab_gdf = diff_ab_gdf[(diff_ab_gdf.geometry.area > 0.0)]
+    diff_ba_gdf = diff_ba_gdf[(diff_ba_gdf.geometry.area > 0.0)]
+
+    # Add colors
+    intersection_style = {'fillColor': intersection_color, 'weight': 2, 'color': 'black', 'fillOpacity': 0.8}
+    diff_ab_style = {'fillColor': diff_ab_color, 'weight': 2, 'color': 'black', 'fillOpacity': 0.8}
+    diff_ba_style = {'fillColor': diff_ba_color, 'weight': 2, 'color': 'black', 'fillOpacity': 0.8}
+    intersection_gdf['style'] = intersection_gdf.apply(lambda x: intersection_style, axis=1)
+    diff_ab_gdf['style'] = diff_ab_gdf.apply(lambda x: diff_ab_style, axis=1)
+    diff_ba_gdf['style'] = diff_ba_gdf.apply(lambda x: diff_ba_style, axis=1)
+
+    # Concatenate results
+    all_gdf = concat([diff_ab_gdf, diff_ba_gdf, intersection_gdf], ignore_index=True, sort=False)
+
+    # Generate map
+    m = map_geometries(all_gdf, tiles=tiles, width=width, height=height)
+
+    return m
+
+
+def map_cluster_contents_osm(cluster_borders, tiles='OpenStreetMap', width='100%', height='100%'):
+    """Constructs a Folium Map displaying the streets and buildings, retreived from OpenStreetMap via OSMNX, within
+    a given AOI.
+
+    Args:
+        cluster_borders (GeoDataFrame): The cluster polygons.
+        tiles (string): The tiles to use for the map (default: `OpenStreetMap`).
+        width (integer or percentage): Width of the map in pixels or percentage (default: 100%).
+        height (integer or percentage): Height of the map in pixels or percentage (default: 100%).
+
+    Returns:
+        A Folium Map object displaying the retreived entities.
+    """
+
+    def is_nan(num):
+        return num != num
+
+    # set the crs to WGS84
+    if cluster_borders.crs['init'] != '4326':
+        cluster_borders = cluster_borders.to_crs({'init': 'epsg:4326'})
+
+    empty_df = DataFrame(columns=['geometry', 'cluster_id', 'type', 'info'])
+    final_gdf = GeoDataFrame(empty_df, crs=cluster_borders.crs, geometry='geometry')
+
+    for index, row in cluster_borders.iterrows():
+        poly = row['geometry']
+        cluster_id = row['cluster_id']
+        osmnx_gdf = osmnx.footprints.create_footprints_gdf(polygon=poly, footprint_type='building',
+                                                           retain_invalid=False)
+        remaining_cols = []
+        columns = list(osmnx_gdf)
+
+        if 'amenity' in columns:
+            remaining_cols.append('amenity')
+
+        if 'building' in columns:
+            remaining_cols.append('building')
+
+        remaining_cols.append('geometry')
+
+        osmnx_gdf = osmnx_gdf[remaining_cols]
+        osmnx_gdf['cluster_id'] = cluster_id
+
+        col_names_2 = list(osmnx_gdf)
+
+        info_list = []
+        for index2, row2 in osmnx_gdf.iterrows():
+            info = ''
+
+            if 'building' in col_names_2:
+                building = row2['building']
+                if not is_nan(building):
+                    if building != 'yes':
+                        info += building + ', '
+
+            if 'amenity' in col_names_2:
+                amenity = row2['amenity']
+                if not is_nan(amenity):
+                    info += amenity
+
+            info_list.append(info)
+
+        del remaining_cols[-1]
+
+        osmnx_gdf['type'] = 'building'
+        osmnx_gdf['info'] = info_list
+        osmnx_gdf.drop(columns=remaining_cols, inplace=True)
+
+        oxg = osmnx.core.graph_from_polygon(poly, network_type='all_private', infrastructure='way["highway"]')
+
+        gdf_edges = osmnx.save_load.graph_to_gdfs(oxg, nodes=False, edges=True, node_geometry=True,
+                                                  fill_edge_geometry=True)
+
+        columns_3 = list(gdf_edges)
+        remaining_cols_3 = []
+
+        if 'highway' in columns_3:
+            remaining_cols_3.append('highway')
+
+        if 'name' in columns_3:
+            remaining_cols_3.append('name')
+
+        if 'oneway' in columns_3:
+            remaining_cols_3.append('oneway')
+
+        remaining_cols_3.append('geometry')
+
+        gdf_edges = gdf_edges[remaining_cols_3]
+        gdf_edges['cluster_id'] = cluster_id
+
+        info_edge_list = []
+        for index2, row3 in gdf_edges.iterrows():
+            info = ''
+
+            if 'name' in columns_3:
+                name = row3['name']
+                if not is_nan(name):
+                    if isinstance(name, list):
+                        info += ','.join(name)
+                    else:
+                        info += name + ', '
+
+            if 'highway' in columns_3:
+                highway = row3['highway']
+                if not is_nan(highway):
+                    if isinstance(highway, list):
+                        info += ','.join(highway)
+                    else:
+                        info += highway + ', '
+
+            if 'oneway' in columns_3:
+                oneway = row3['oneway']
+                if not is_nan(oneway):
+                    info += 'oneway'
+
+            info_edge_list.append(info)
+
+        del remaining_cols_3[-1]
+        gdf_edges['type'] = 'Road'
+        gdf_edges['info'] = info_edge_list
+        gdf_edges.drop(columns=remaining_cols_3, inplace=True)
+
+        res_gdf = concat([osmnx_gdf, gdf_edges], axis=0, join='outer', ignore_index=False,
+                         keys=None, levels=None, names=None, verify_integrity=False, copy=True)
+        final_gdf = concat([final_gdf, res_gdf], axis=0, join='outer', ignore_index=False,
+                           keys=None, levels=None, names=None, verify_integrity=False, copy=True)
+
+        final_gdf.reset_index(inplace=True, drop=True)
+
+    m = map_geometries(final_gdf, tiles=tiles, width=width, height=height)
 
     return m
